34911ccabbe8f6cf958dcedb68628f3bc483678f
Michael Davison <mdavison92@Hotmail.com>
2021-07-05 14:40:51 -0400

    Create ShareReportForm

---
+++ src/components/ShareReport.js
@@ -0,0 +1,63 @@
+import React, {Component, useState} from "react";
+import '../css/App.css'
+import '../css/ShareReportForm.css'
+import closeButton from "../resources/close-button.png"
+
+const ShareReport = (props) => {
+
+   const getYear = () => {
+        return new Date().getFullYear();
+    }
+
+    const [currentYear, setCurrentYear] = useState(getYear())
+    const [email, setEmail] = useState(0);
+    const [reportNotes, setReportNotes] = useState(0);
+
+
+    const handleEmailChange = event => {
+        let emailInput = event.target.value;
+        setEmail(emailInput);
+      };
+
+      const handleNotesChange = event => {
+        let reportNotesInput = event.target.value;
+        setReportNotes(reportNotesInput);
+      };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        //validate email call
+        props.formHandler(email, reportNotes);
+        
+    }
+
+    return (
+        <div className="share-report-container">
+        
+        <button className="share-report-exit-btn"><img src ={closeButton} alt=""></img></button>
+        <form className ="share-report-form" type="submit" value="Submit">
+            
+            <h2>Primary Report {currentYear}</h2>
+            <label>Enter email addresses separated by coma {"\n"}</label>
+            <input id="email" type="email" className="email-input"placeholder="John.doe@pharmacy.net" onChange={handleEmailChange} required></input>
+            {"\n"}
+            <label>Add a note</label>
+            {"\n"}
+            <textarea id="reportNotes"className ="report-notes-input" placeholder="Optional" onChange={handleNotesChange} required></textarea>
+            {"\n"}
+            <div className="share-report-form-btns">
+                <button className="cancel-btn">Cancel</button>
+                <button className="share-report-submit-btn" type="submit" onClick={handleSubmit}>Share Report</button>
+            </div>
+        </form>
+
+        </div>
+        
+    );
+
+
+};
+
+
+
+export default ShareReport;
